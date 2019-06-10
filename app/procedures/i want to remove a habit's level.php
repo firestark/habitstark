13dbabc709d71e9bfe1f0016f001fd8c23472943
Aron Wouters <lionar@mail.com>
2019-06-10 15:44:00 +0200

    Corrected payload parameter name

--- app/procedures/i want to remove a habit's level.php
+++ app/procedures/i want to remove a habit's level.php
@@ -7,5 +7,5 @@ function ( int $number, goal\habit $habit, goal $goal, goal\manager $manager )
     $goal->habits [ $habit->id ]->remove ( $number );
     $manager->update ( $goal );
 
-    return [ 1012, [ 'goalid' => $goal->id, 'habit' => $habit->id ] ];
+    return [ 1012, [ 'goalid' => $goal->id, 'habitid' => $habit->id ] ];
 } ) ) ) );

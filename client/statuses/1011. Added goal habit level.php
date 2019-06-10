66523c5b3519937a6c07afc3c3c80aeeb0116d29
Aron Wouters <lionar@mail.com>
2019-06-10 18:59:07 +0200

    Added flash message

--- client/statuses/1011. Added goal habit level.php
+++ client/statuses/1011. Added goal habit level.php
@@ -4,5 +4,6 @@ use function compact as with;
 
 status::matching ( 1011, function ( $goalid, $habitid )
 {
+   session::flash ( 'message', 'Habit level added.' );
    return redirect::to ( '/' . $goalid . '/habits/' . $habitid . '/levels' );
 } );

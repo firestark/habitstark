6ae918526b6b9ecc50d3e85f19d70507e7ae7c3b
Aron Wouters <lionar@mail.com>
2019-06-12 10:38:40 +0200

    Removed blank lines

--- client/views/goals/habits/levels/add.blade.php
+++ client/views/goals/habits/levels/add.blade.php
@@ -9,7 +9,6 @@
 @endsection
 
 @section ( 'content' )
-
     <form method="POST" style="display: grid; height: 100%;" action="/{{ $goalid }}/habits/{{ $habitid }}/levels">
         <textarea class="mdc-typography--body1" name="description" placeholder="Description" style="width: 100%; height: 100%; box-sizing: border-box; resize: none; border: none; padding: 24px;"></textarea>
     
